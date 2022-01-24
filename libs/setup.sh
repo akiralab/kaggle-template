c4afa2164d49f24602a8a783385b811adc42947f
Akira Kawai <kawaiakira51@gmail.com>
2022-01-24 11:33:25 +0900

    updated initial state

---
+++ libs/setup.sh
@@ -0,0 +1,8 @@
+mkdir res
+touch requirements.txt
+
+
+# 画像系のコンペの場合、
+# 画像系モデルが利用可能なライブラリtimmを導入する
+# git clone git@github.com:rwightman/pytorch-image-models.git
+# cd python-image-models && git checkout f83b0b0
